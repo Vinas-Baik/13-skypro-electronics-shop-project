2343265c9b988a61898d03921b1518d0c8420a9a
Victor <123475829+Vinas-Baik@users.noreply.github.com>
2023-08-10 17:53:45 +0800

    Update main.py

--- homework-3/main.py
+++ homework-3/main.py
@@ -1,9 +1,16 @@
 from src.item import Item
 
 if __name__ == '__main__':
+    #
+    # НАЧАЛО программы
+    #
 
     item1 = Item("Смартфон", 10000, 20)
     print(repr(item1))
     print(str(item1))
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
     assert str(item1) == 'Смартфон'
+
+    #
+    # КОНЕЦ программы
+    #
