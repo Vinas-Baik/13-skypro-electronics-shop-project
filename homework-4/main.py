3afdaf04e708bcf84bd68a4ca16fdffafa2f9853
Victor Sadykov <svn1409@gmail.com>
2023-08-14 14:41:23 +0800

    home work 4 - навели красоту

--- homework-4/main.py
+++ homework-4/main.py
@@ -1,4 +1,4 @@
-from src.item import Item
+from src.item import Item, nice_number_output
 from src.phone import Phone
 
 if __name__ == '__main__':
@@ -16,13 +16,20 @@ if __name__ == '__main__':
     assert phone1.number_of_sim == 2
     print()
     item1 = Item("Смартфон", 10000, 20)
-    print(item1.my_str())
-    print(phone1.my_str())
+    for item in Item.all:
+        print(f'\033[33m{item.name}\033[39m '
+              f'в количестве \033[33m{item.quantity} шт.\033[39m '
+              f'по цене \033[32m{nice_number_output(item.price)} руб.\033[39m '
+              f'Итоговая сумма = '
+              f'\033[32m{nice_number_output(item.calculate_total_price())}'
+              f' руб.\033[39m')
     print()
     assert item1 + phone1 == 25
-    print(f'Количество устройств {item1} и {phone1} = {item1 + phone1}')
+    print(f'Количество устройств \033[33m{item1}\033[39m и '
+          f'\033[33m{phone1}\033[39m = \033[32m{item1 + phone1}\033[39m')
     assert phone1 + phone1 == 10
-    print(f'Количество устройств {phone1} и {phone1} = {phone1 + phone1}')
+    print(f'Количество устройств \033[33m{phone1}\033[39m и '
+          f'\033[33m{phone1}\033[39m = \033[32m{phone1 + phone1}\033[39m')
     print()
     print(repr(phone1))
     print()
@@ -32,13 +39,13 @@ if __name__ == '__main__':
         phone1.number_of_sim = 0
     except ValueError as v_err:
         # ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.
-        print(v_err.args[0])
+        print(f'\033[31m{v_err.args[0]}\033[39m')
 
     print('Установим количество SIM-карт = 0.1 ')
     try:
         phone1.number_of_sim = 0.1
     except ValueError as v_err:
-        print(v_err.args[0])
+        print(f'\033[31m{v_err.args[0]}\033[39m')
 
     print()
     print('Установим количество SIM-карт = 10 ')
