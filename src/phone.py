cd38d1c3d6434cc2ec31cb8b1a00b8ae36cd7c3c
Victor Sadykov <svn1409@gmail.com>
2023-08-14 11:13:45 +0800

    home work 4

--- src/phone.py
+++ src/phone.py
@@ -3,6 +3,9 @@ from src.item import Item
 class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity=1, number_of_sim=1):
+        """
+        Магический метод для инициализации объекта класса с вызовом магического метода родительского класса
+        """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
@@ -12,8 +15,13 @@ class Phone(Item):
 
     @number_of_sim.setter
     def number_of_sim(self, n_sim):
+        """
+        Сеттер для переменной number_of_sim с проверкой на целое и положительное число
+        """
+        # проверяем на целое число
         if not isinstance(n_sim, int):
             raise ValueError('Количество физических SIM-карт должно быть целым числом')
+        # проверяем на положительное число
         if n_sim < 1:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
         self.__number_of_sim = n_sim
