337c8d2363d93b037eb7ba9bd41abb8a7daedd53
Victor Sadykov <svn1409@gmail.com>
2023-08-16 20:19:17 +0800

    home work 5 - keyboard

--- homework-5/main.py
+++ homework-5/main.py
@@ -1,4 +1,6 @@
 from src.keyboard import Keyboard
+from src.phone import Phone
+from src.item import Item, nice_number_output
 
 if __name__ == '__main__':
 
@@ -7,6 +9,8 @@ if __name__ == '__main__':
     #
 
     kb = Keyboard('Dark Project KD87A', 9600, 5)
+    print(repr(kb))
+    print()
     assert str(kb) == "Dark Project KD87A"
 
     assert str(kb.language) == "EN"
@@ -31,6 +35,19 @@ if __name__ == '__main__':
         # AttributeError: property 'language' of 'Keyboard' object has no setter
     except ValueError as v_err:
         print(f'\033[31m{v_err.args[0]}\033[39m')
+    print()
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    for item in Item.all:
+        print(f'\033[33m{item.name}\033[39m '
+              f'в количестве \033[33m{item.quantity} шт.\033[39m '
+              f'по цене \033[32m{nice_number_output(item.price)} руб.\033[39m '
+              f'Итоговая сумма = '
+              f'\033[32m{nice_number_output(item.calculate_total_price())}'
+              f' руб.\033[39m')
+    print()
+    assert kb + phone1 == 10
+    print(f'Количество устройств \033[33m{kb}\033[39m и '
+          f'\033[33m{phone1}\033[39m = \033[32m{kb + phone1}\033[39m')
 
     #
     # КОНЕЦ ПРОГРАММЫ
