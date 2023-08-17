d3093f36e5e1ee52e690ea05ead652cdbd2a5808
Victor Sadykov <svn1409@gmail.com>
2023-08-17 08:28:31 +0800

    home work 5 - add test

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -7,12 +7,12 @@ def test_class_phone():
     temp_kb = my_kb.Keyboard('Logitech', 2000, 5)
     assert temp_kb.language == 'EN'
     assert repr(temp_kb) == "Keyboard('Logitech', 2000, 5, EN)"
-    with pytest.raises(ValueError):
-        temp_kb = my_kb.Keyboard('Logitech', 2000, 5, 'СH')
 
-    temp_kb = my_kb.Keyboard('Logitech', 2000, 10, 'RU')
-    assert temp_kb.language == 'RU'
-    assert temp_kb.change_lang().language == 'EN'
-    assert temp_kb.change_lang().change_lang().language == 'EN'
-    assert temp_kb.my_str() == 'Logitech в количестве 10 шт по цене 2 000 руб. ' \
-                               '- итоговая сумма 20 000 руб., установлен язык - EN'
+    assert temp_kb.change_lang().language == 'RU'
+    assert temp_kb.change_lang().change_lang().language == 'RU'
+    assert temp_kb.my_str() == 'Logitech в количестве 5 шт по цене 2 000 руб. ' \
+                               '- итоговая сумма 10 000 руб., установлен язык - RU'
+
+    for t_lang in ('12    ', '   ch', ' '):
+        with pytest.raises(ValueError):
+            temp_kb.language = t_lang
